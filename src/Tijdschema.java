e594bde498b0204a136d0e335a949249a560599b
jeanjanssens <jeanjanssens@w229h179.wireless.ugent.be>
2018-12-17 00:40:42 +0100

    aanpassingen aan ui en tijdschema beginnen schrijven

--- src/Tijdschema.java
+++ src/Tijdschema.java
@@ -1,6 +1,8 @@
 
 import java.time.LocalDateTime;
 import java.time.Year;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,5 +21,13 @@ public class Tijdschema {
   private LocalDateTime capaciteitDeadline;
   private LocalDateTime voorkeurDeadline;
   private LocalDateTime datum;
+  
+  public void veranderTijd(){
+      try {
+          DBTijdschema.setDatum(jaar,startDatum,inschrijvingenDeadline, capaciteitDeadline, voorkeurDeadline, datum);
+      } catch (DBException ex) {
+          ex.getMessage();
+      }
+  }
   
 }
